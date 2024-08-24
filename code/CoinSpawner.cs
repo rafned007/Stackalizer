d868062034071ee52f54c2ca7656d1a96ce87698
Noneya Bisnus <cmkroop@gmail.com>
2024-08-24 00:36:40 -0400

    (coin spawn timer fix)

--- code/CoinSpawner.cs
+++ code/CoinSpawner.cs
@@ -8,32 +8,26 @@ public sealed class CoinSpawner : Component
 	[Property] public GameObject pinkCoin {get; set;}
 	[Property] public GameObject spawnLocation {get; set;}
 
-	private TimeUntil doClone;
 
 	protected override void OnStart()
 	{
-		doClone = TimetillClone;
-		Log.Info(doClone == true);
-
-		var cloneRC = redCoin.Clone(Transform.Position);
-		var cloneBC = blueCoin.Clone(Transform.Position);
-		var clonePC = pinkCoin.Clone(Transform.Position);
-
-		if(doClone)
-		{
-			Log.Info(Transform.Position);
-			cloneRC.Transform.Position = spawnLocation.Transform.Position;
-			cloneBC.Transform.Position = spawnLocation.Transform.Position;
-			clonePC.Transform.Position = spawnLocation.Transform.Position;
-			doClone = 5;
-		}
+		TimetillClone = 5f;
+		coinspawn();
 	}
 	
 	
-	protected override void OnUpdate()
+	async void coinspawn()
 	{
-		
-		Log.Info(doClone == true);
+		await Task.DelaySeconds(TimetillClone);
+		var cloneRC = redCoin.Clone(Transform.Position);
+		var cloneBC = blueCoin.Clone(Transform.Position);
+		var clonePC = pinkCoin.Clone(Transform.Position);
+		cloneagain();
+
+	}
+	void cloneagain()
+	{
+		coinspawn();
 	}
 
 	// protected override void OnStart()
